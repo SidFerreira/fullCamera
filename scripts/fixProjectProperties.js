debb6efd5fa41067b86b240bb349b1a7d39bc8fd
Sidney Ferreira <sid@ferreiraz.com.br>
2015-02-22 13:57:54 -0300

    Fixing script to run android properly

--- scripts/fixProjectProperties.js
+++ scripts/fixProjectProperties.js
@@ -4,8 +4,8 @@ var path = require('path');
 var config = fs.readFileSync(process.env.PWD + '/config.xml', { encoding: 'utf8' });
 var projectName = config.match(/id="([^"]+)"/g).pop().split(".").pop().split('"').join('');
 var propertiesPath = process.env.PWD + '/platforms/android/br.com.ferreiraz.fullCamera/' + projectName + '-fullcamera/project.properties';
+var propertiesFile = fs.readFileSync(propertiesPath, { encoding: 'utf8' });
 if(propertiesFile) {
-	var propertiesFile = fs.readFileSync(propertiesPath, { encoding: 'utf8' });
 	propertiesFile = propertiesFile.replace(/{{projectName}}/g, projectName);
 	fs.writeFileSync(propertiesPath, propertiesFile, { encoding: 'utf8' });	
 }
