b0fed0a6ed8e655fe561f51d0a07e4faafd320d7
Sidney Ferreira <sid@ferreiraz.com.br>
2015-03-05 06:47:44 -0300

    Debuging messages

--- scripts/fixProjectProperties.js
+++ scripts/fixProjectProperties.js
@@ -1,10 +1,14 @@
 var fs = require('fs');
 var path = require('path');
 
+console.log('>>>>>>>>>>>>>>>>>>>>>>>>>');
+console.log('Cordova Full Camera: Fixing properties file.');
 var config = fs.readFileSync(process.env.PWD + '/config.xml', { encoding: 'utf8' });
 var projectName = config.match(/id="([^"]+)"/g).pop().split(".").pop().split('"').join('');
 var propertiesPath = process.env.PWD + '/platforms/android/br.com.ferreiraz.fullCamera/' + projectName + '-fullcamera/project.properties';
 var propertiesFile = fs.readFileSync(propertiesPath, { encoding: 'utf8' });
+console.log('process.env.PWD: ' + process.env.PWD);
+console.log('propertiesFile: ' + propertiesPath);
 if(propertiesFile) {
 	propertiesFile = propertiesFile.replace(/{{projectName}}/g, projectName);
 	fs.writeFileSync(propertiesPath, propertiesFile, { encoding: 'utf8' });	
@@ -14,3 +18,4 @@ if(propertiesFile) {
 	console.log(process.env);
 	process.exit();
 }
+console.log('>>>>>>>>>>>>>>>>>>>>>>>>>');
