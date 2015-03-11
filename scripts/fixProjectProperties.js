900a1f0b032f297bc57fbf8a4012c0a4787a81af
Sidney Ferreira <sid@ferreiraz.com.br>
2015-03-11 13:23:14 -0300

    Properties file fix fixed

--- scripts/fixProjectProperties.js
+++ scripts/fixProjectProperties.js
@@ -1,22 +1,20 @@
 var fs = require('fs');
 var path = require('path');
 
-console.log('>>>>>>>>>>>>>>>>>>>>>>>>>');
-console.log('Cordova Full Camera: Fixing properties file.');
 var config = fs.readFileSync(process.env.PWD + '/config.xml', { encoding: 'utf8' });
 var projectName = config.match(/id="([^"]+)"/g).pop().split(".").pop().split('"').join('');
 var propertiesPath = process.env.PWD + '/platforms/android/br.com.ferreiraz.fullCamera/' + projectName + '-fullcamera/project.properties';
 var propertiesFile = fs.readFileSync(propertiesPath, { encoding: 'utf8' });
-console.log('process.env.PWD: ' + process.env.PWD);
-console.log('propertiesPath: ' + propertiesPath);
-console.log('propertiesFile: ' + propertiesFile);
+
 if(propertiesFile) {
+	console.log('FerreiraZ >>> Fixing properties file.');
 	propertiesFile = propertiesFile.replace(/{{projectName}}/g, projectName);
 	fs.writeFileSync(propertiesPath, propertiesFile, { encoding: 'utf8' });	
 } else {
+	console.log('------ FerreiraZ');
 	console.log('Error fixing properties file.');
 	console.log('process.env.PWD may be undefined. Showing process.env:');
 	console.log(process.env);
+	console.log('------');
 	process.exit();
 }
-console.log('>>>>>>>>>>>>>>>>>>>>>>>>>');
